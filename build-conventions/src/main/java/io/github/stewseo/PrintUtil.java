e3064939ac5a7dcef3d79a2fd356003992d18481
Stew Seo <seostew2@gmail.com>
2023-01-02 07:23:20 -0800

    Add filters that match bug categories and bug patterns in packages for /config/spotbugs-include.xml

--- build-conventions/src/main/java/io/github/stewseo/PrintUtil.java
+++ build-conventions/src/main/java/io/github/stewseo/PrintUtil.java
@@ -12,24 +12,16 @@ public class PrintUtil {
 
     public static final String RED = "\u001B[31m";
 
-    public static <T> String green(T title) {
-        return GREEN + title + RESET;
+    public static <T> void printGreen(T title) {
+        System.out.print(GREEN + title + RESET);
     }
 
-    public static <T> String red(T title) {
-        return RED + title + RESET;
+    public static <T> void printRed(T title) {
+        System.out.print(RED + title + RESET);
     }
 
-    public static <T> String debug(T title) {
-        return (CYAN + title + RESET);
-    }
-
-    public static <T> String tracer(T title) {
-        return (RED + title + RESET);
-    }
-
-    public static <T> String cyan(T title) {
-        return CYAN + title + RESET;
+    public static <T> void printCyan(T title) {
+        System.out.print(CYAN + title + RESET);
     }
 
     public static <T> T print(T t) {
@@ -37,19 +29,17 @@ public class PrintUtil {
         return t;
     }
 
-//    public static void title(Object title) {
-//        println("");
-//        println("-".repeat(("" + title).length()));
-//    }
-
-//    public static <T> T println(T t) {
-//        System.out.println(t);
-//        return t;
-//    }
-//
-//    public static <T> T println(String description, T t) {
-//        System.out.print("description: " + description + " ");
-//        println(t);
-//        return t;
-//    }
+    public static void title(Object title) {
+        println("");
+        println("-".repeat(("" + title).length()));
+    }
+    public static <T> T println(String description, T t) {
+        System.out.print("description: " + description + " ");
+        println(t);
+        return t;
+    }
+    public static <T> T println(T t) {
+        System.out.println(t);
+        return t;
+    }
 }
