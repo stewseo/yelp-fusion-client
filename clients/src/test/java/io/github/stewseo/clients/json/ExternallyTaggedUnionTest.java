d22b2c42ce71ebf4173384487ed188e47620a6b1
Stew Seo <seostew2@gmail.com>
2023-01-04 12:30:39 -0800

    local

---
+++ clients/src/test/java/io/github/stewseo/clients/json/ExternallyTaggedUnionTest.java
@@ -0,0 +1,24 @@
+package io.github.stewseo.clients.json;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExternallyTaggedUnionTest {
+
+    @Test
+    void arrayMapDeserializer() {
+    }
+
+    @Test
+    void serializeTypedKeys() {
+    }
+
+    @Test
+    void serializeTypedKeysArray() {
+    }
+
+    @Test
+    void serializeTypedKeysInner() {
+    }
+}
